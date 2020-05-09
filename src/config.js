b5ea5f98b869a5d08208f404981e355e6b15a7b0
Luke Wolf <lukewolfgames@gmail.com>
2020-05-09 22:44:40 +1000

    first commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import "phaser";
+
+
+
+const config = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
